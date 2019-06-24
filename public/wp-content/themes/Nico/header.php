fa0771c310ca7daa26a1ee779bfd113dab472ef8
nicomesa88 <nicomesa88@gmail.com>
2019-06-24 11:15:22 -0500

    adding code into the header and footer

--- public/wp-content/themes/Nico/header.php
+++ public/wp-content/themes/Nico/header.php
@@ -1,1 +1,11 @@
-<!-- This is the file for the header -->
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+        <header>
+        </header>
