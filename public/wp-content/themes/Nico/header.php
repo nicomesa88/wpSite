8e74c12faa6e7f743bcaa6c81bd6f5890ed45420
nicomesa88 <nicomesa88@gmail.com>
2019-07-01 12:09:10 -0500

    adding mine nave to header

--- public/wp-content/themes/Nico/header.php
+++ public/wp-content/themes/Nico/header.php
@@ -1,3 +1,17 @@
+<?php 
+global $post;
+
+$menuLocations = get_nav_menu_locations();
+$menuID = $menuLocations['main-menu'];
+$menuitems = wp_get_nav_menu_items( $menuID, array( 'order' => 'DESC' ) );
+$submenu = false;
+$sub_parent = false;
+$no_sub_parent = false;
+$sub_parent_ID;
+$count = 0;
+
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
@@ -9,4 +23,21 @@
 	<body <?php body_class(); ?>>
         <header>
         <h1>This is the header</h1>
+        <nav>
+            <ul>
+                <?php
+                    foreach($menuitems as $item){
+                        $link = $item->url;
+                        $title = $item->title;
+                        $next_item_parent = $menuitems[$count + 1]->menu_item_parent;
+
+                        if( !$item->menu_item_parent){
+                            $parent_id = $item->ID;
+                            $full_nav = get_field('full_width', $item); ?>
+                            <li class="<?php if($full_nav){ echo'full_menu '; } if(site_url().$_SERVER['REQUEST_URI']==$link){ echo'active'; } ?>"><a href="<?php echo $link; ?>" class="<?php if(site_url().$_SERVER['REQUEST_URI']==$link){ echo'active'; } ?>"><?php echo $title; ?></a>  
+                        <?php } 
+                    }
+                ?>    
+            </ul>
+        </nav>
         </header>
