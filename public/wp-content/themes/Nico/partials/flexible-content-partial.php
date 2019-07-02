2d6294cb4626636aca9a46d6436e5e9638c376dd
nicomesa88 <nicomesa88@gmail.com>
2019-07-02 10:47:09 -0500

    removing divs/fixing to display image from array

--- public/wp-content/themes/Nico/partials/flexible-content-partial.php
+++ public/wp-content/themes/Nico/partials/flexible-content-partial.php
@@ -5,33 +5,33 @@ if( have_rows('content') ):
 
      // loop through the rows of data
     while ( have_rows('content') ) : the_row(); ?>
-    
-    <div>
+
         <?php if( get_row_layout() == 'text' ):?>
         <div>
             <?php the_sub_field('text');
             the_sub_field('text_2'); ?>
         </div>
         <?php endif; ?>
-    </div>
-
-    <div>
-        <?php if( get_row_layout() == 'image' ): ?>
+   
+        <?php if( get_row_layout() == 'image' ): 
+        $image = get_sub_field('image');
+        // echo'<pre>';
+        // var_dump($image);
+        // echo '</pre>';
+        ?>
         <div>
-            <img src="<?php the_sub_field('image');?>">
+            <img src="<?php echo $image['sizes']['thumbnail'];?>">
             <?php the_sub_field('caption'); ?>
         </div>
         <?php endif; ?>
-    </div>
-
-    <div>
+    
         <?php if( get_row_layout() == 'quote' ): ?>
         <div>
             <?php the_sub_field('quote');
             the_sub_field('name'); ?>
         </div>
        <?php endif; ?>
-    </div>
+       
     <?php endwhile;
 
 else :
